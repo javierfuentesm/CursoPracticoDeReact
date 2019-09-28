8462f0b7b3d012550d57365412a313cc3f824724
javierfuentesm <javier.fuentesm@hotmail.com>
2019-09-28 17:28:00 -0500

    Pops

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import HolaMundo from './componenets/HolaMundo'
+import HolaMundo from './componenets/HolaMundo';
+import Button from './componenets/Button'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<HolaMundo />, document.getElementById('root'));
+ReactDOM.render(<Button text="click 2" />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
